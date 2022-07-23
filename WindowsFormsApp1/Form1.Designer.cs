3e1e3415f3bc247c98057e5a483312a48997f765
darkholy0309 <58006469+darkholy0309@users.noreply.github.com>
2022-07-23 16:42:09 +0900

    Initial commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.webBrowser2 = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(56, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(12, 24);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(250, 250);
+            this.webBrowser1.TabIndex = 2;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // webBrowser2
+            // 
+            this.webBrowser2.Location = new System.Drawing.Point(268, 24);
+            this.webBrowser2.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser2.Name = "webBrowser2";
+            this.webBrowser2.Size = new System.Drawing.Size(250, 250);
+            this.webBrowser2.TabIndex = 3;
+            this.webBrowser2.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser2_DocumentCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.webBrowser2);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.WebBrowser webBrowser2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen;
+            MaximizeBox = false;
+            AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            Size = new Size(610, 110);
+            //http://blog.naver.com/totaes2/221248783837
+            //http://gamebulletin.nexon.com/maplestory/inspection3.html
+            //http://gamebulletin.nexon.com/maplestory/game3.html
+            label1.Location = new Point(10, 10);
+            label2.Location = new Point(10, 40);
+            label1.Font = new Font("dotum", 14);
+            label2.Font = new Font("dotum", 14);
+            webBrowser1.Visible = false;
+            webBrowser2.Visible = false;
+            webBrowser1.ScriptErrorsSuppressed = true;
+            webBrowser2.ScriptErrorsSuppressed = true;
+            main();
+        }
+
+        void main()
+        {
+            if (Convert.ToInt32(Microsoft.Win32.RegistryKey.OpenBaseKey(Microsoft.Win32.RegistryHive.LocalMachine, Microsoft.Win32.RegistryView.Default).OpenSubKey("software\\microsoft\\windows nt\\currentversion").GetValue("currentbuild")) < 22000)
+            {
+                MessageBox.Show("windows 11 update 21H2" + Environment.NewLine + "윈도우 업데이트 필요");
+            }
+            if (System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable())
+            {
+                webBrowser1.Navigate("https://maplestory.nexon.com/news/notice/inspection");
+                webBrowser2.Navigate("https://maplestory.nexon.com/testworld/main");
+            }
+            else
+            {
+                MessageBox.Show("인터넷확인");
+                Environment.Exit(0);
+            }
+        }
+
+        private void webBrowser2_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            if (webBrowser2.Url.AbsoluteUri == e.Url.AbsoluteUri)
+            {
+                foreach (HtmlElement htmlelement in webBrowser2.Document.GetElementsByTagName("div"))
+                {
+                    if (htmlelement.GetAttribute("className") == "test_wrold_side")
+                    {
+                        string text = null;
+                        if (htmlelement.InnerText.Trim().Substring(0, 14) == "테스트월드 운영기간입니다.")
+                        {
+                            foreach (HtmlElement htmlelement2 in webBrowser2.Document.GetElementsByTagName("div"))
+                            {
+                                if (htmlelement2.GetAttribute("className") == "contents_wrap")
+                                {
+                                    if (htmlelement2.Children[2].Children[1].Children[0].Children[1].InnerText.Trim().Substring(0, 4) == "[오픈]")
+                                    {
+                                        text = htmlelement2.Children[2].Children[1].Children[0].Children[1].InnerText.Trim();
+                                    }
+                                    if (htmlelement2.Children[2].Children[1].Children[1].Children[1].InnerText.Trim().Substring(0, 4) == "[오픈]")
+                                    {
+                                        text = htmlelement2.Children[2].Children[1].Children[1].Children[1].InnerText.Trim();
+                                    }
+                                    if (htmlelement2.Children[2].Children[1].Children[2].Children[1].InnerText.Trim().Substring(0, 4) == "[오픈]")
+                                    {
+                                        text = htmlelement2.Children[2].Children[1].Children[2].Children[1].InnerText.Trim();
+                                    }
+                                    break;
+                                }
+                            }
+                        }
+                        label2.Text = text;
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            if (webBrowser1.Url.AbsoluteUri == e.Url.AbsoluteUri)
+            {
+                foreach (HtmlElement htmlelement in webBrowser1.Document.GetElementsByTagName("div"))
+                {
+                    if (htmlelement.GetAttribute("className") == "news_board")
+                    {
+                        string text = null;
+                        if (htmlelement.Children[0].Children[0].Children[0].InnerText.Trim().Substring(0, 9) == "(연장)[패치중]")
+                        {
+                            text = htmlelement.Children[0].Children[0].Children[0].InnerText.Trim();
+                        }
+                        if (htmlelement.Children[0].Children[0].Children[0].InnerText.Trim().Substring(0, 6) == "[패치예정]")
+                        {
+                            text = htmlelement.Children[0].Children[0].Children[0].InnerText.Trim();
+                        }
+                        if (htmlelement.Children[0].Children[0].Children[0].InnerText.Trim().Substring(0, 5) == "[패치중]")
+                        {
+                            text = htmlelement.Children[0].Children[0].Children[0].InnerText.Trim();
+                        }
+                        if (htmlelement.Children[0].Children[0].Children[0].InnerText.Trim().Substring(0, 9) == "(연장)[점검중]")
+                        {
+                            text = htmlelement.Children[0].Children[0].Children[0].InnerText.Trim();
+                        }
+                        if (htmlelement.Children[0].Children[0].Children[0].InnerText.Trim().Substring(0, 6) == "[점검예정]")
+                        {
+                            text = htmlelement.Children[0].Children[0].Children[0].InnerText.Trim();
+                        }
+                        if (htmlelement.Children[0].Children[0].Children[0].InnerText.Trim().Substring(0, 5) == "[점검중]")
+                        {
+                            text = htmlelement.Children[0].Children[0].Children[0].InnerText.Trim();
+                        }
+                        label1.Text = text;
+                        if (text == null)
+                        {
+                            label1.Text = "공지없음";
+                        }
+                        label1.Text = htmlelement.Children[0].Children[0].Children[0].InnerText.Trim();//test view
+                        break;
+                    }
+                    if (htmlelement.GetAttribute("className") == "information")
+                    {
+                        label1.Text = htmlelement.Children[0].InnerText.Trim();
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
